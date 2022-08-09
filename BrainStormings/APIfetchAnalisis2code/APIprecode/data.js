90e6b3d2e633b70af431fde9f830f134ffd0b174
FrankTII <frank.training2@gmail.com>
2022-08-08 20:57:46 -0500

    data.js : imprime los registros de todos los JSONS que recibe una receta o una serie de recetas o categorias Ya se probo si el arreglo regresa una o varias recetas en el JSON. Se probaron mas busquedas. Solo falla en las imagenes que se procesas de forma diferente y si tiene selecionado un tipo de dato categories o meals. Solo la busqueda ALLMealCats regresa algo diferente porque es un OBJ CATEGORIES mientras todos los demas rergesan meals

--- BrainStormings/APIfetchAnalisis2code/APIprecode/data.js
+++ BrainStormings/APIfetchAnalisis2code/APIprecode/data.js
@@ -7,6 +7,7 @@ function callMealDBAPI(url,callType){
     })
     .then(function(data) {
         console.log(data);
+        let jsonHTML="";//Elementop que carga el html en el punto indicado
         //Marcadores usados en la aproximacion al codigo
         //Borrar si ya tenemos una funcion
         // console.log(a=1);
@@ -17,16 +18,20 @@ function callMealDBAPI(url,callType){
         // data_json={...data.meals[0]};
         // console.log(a=1);
         let size = Object.keys(data.meals).length;
-        console.log('tamaño del Obj data: '+size);
-        let jsonHTML="";
-
+        // let size = Object.keys(data.categories).length;
+        
+        let msj=`${'<h2>Número de elementos en el OBJ(tamaño del Obj data): '+size+'</h2>'}`;
+        console.log(msj);
+        jsonHTML+=msj;
         //For el numero de meals en el objeto pasar cada uno y que los imprima en pagina
-        console.log('Vamos a entrar al for que pasa el index')
+        console.log('Antes de entrar al for que recibe el index igual al tamaño del OBJ')
         for (let mealsObjIndex = 0; mealsObjIndex < size; mealsObjIndex++) {
         // Este es el objeto json de un solo arrreglo que si lo paso a la funcion como esta si funciona y se imprime en console log y en  html code
         // data_json=Object.entries(data.meals[0]);
 
             data_json=Object.entries(data.meals[mealsObjIndex]);
+            // data_json=Object.entries(data.categories[mealsObjIndex]);
+
             console.log(mealsObjIndex);//marcador de fase en que indice voy del arreglo
             console.log(data_json);//despliega el contenido pasado
             //Esto se debe borrar en cuanto la funcion jale
@@ -38,8 +43,12 @@ function callMealDBAPI(url,callType){
             //     jsonHTML+=iterator+"<br>";
             // }
             
-            console.log('Indice:'+mealsObjIndex+' Longitud del arreglo: '+data_json.length);
-            jsonHTML+=jsonHTML+`${'<p>Indice:'+ mealsObjIndex+' Longitud del arreglo: '+data_json.length+'</p>'}`;
+            msj=`${'<p>El elemento['+mealsObjIndex+'] tiene una longitud de subarreglo: '+data_json.length+'</p>'}`;
+            console.log(msj);
+            jsonHTML+=msj;
+            // jsonHTML+=jsonHTML+`${'<p>Indice:'+ mealsObjIndex+' Longitud del arreglo: '+data_json.length+'</p>'}`;
+
+            
 
             for (let i = 0; i < data_json.length; i++) {
                 
@@ -63,39 +72,70 @@ function callMealDBAPI(url,callType){
 let callType='GET';
 let searchxMealName='https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata';
 
+//.meals uncomment. Es un Obj: meals
+// console.log(callMealDBAPI(searchxMealName, callType));
+// callMealDBAPI(searchxMealName, callType);
+
 let listAllMealsx1stLetter='https://www.themealdb.com/api/json/v1/1/search.php?f=a';
+
+//.meals uncomment. Es un Obj: meals
+// console.log(callMealDBAPI(listAllMealsx1stLetter,callType));
 
 // ###
 // # Lookup full meal details by id
 let searchMealxId='https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772';
 
+//.meals uncomment. Es un Obj: meals
+// console.log(callMealDBAPI(searchMealxId,callType));
+
 // ###
 // # Lookup a single random meal
 let searchSingleRandomMeal='https://www.themealdb.com/api/json/v1/1/random.php';
+
+//.meals uncomment. Es un Obj: meals
+// console.log(callMealDBAPI(searchSingleRandomMeal,callType));
 
 // ###
 // # Lookup a selection of 10 random meals (only available to $2+ Paypal supporters)
 let search10RandomMeals='https://www.themealdb.com/api/json/v1/1/randomselection.php';
 
+//.meals uncomment. Es un Obj: meals
+//No se prueba hasta comprar API
+// console.log(callMealDBAPI(search10RandomMeals,callType));
+
 // ###
 // # List all meal categories
 let searchAllMealCats='https://www.themealdb.com/api/json/v1/1/categories.php';
+
+//.categories uncomment. Es un OBJ: CATEGORIES
+// console.log(callMealDBAPI(searchAllMealCats,callType));
 
 // ###
 // # Latest Meals (only available to $2+ Paypal supporters)
 
 let searchLatestMeal='https://www.themealdb.com/api/json/v1/1/latest.php';
 
+//.meals uncomment. Es un Obj: meals
+// console.log(callMealDBAPI(searchLatestMeal,callType));
+
 // ###
 // # List all Categories, Area, Ingredients
 let searchAllCats='https://www.themealdb.com/api/json/v1/1/list.php?c=list';
 
+//.meals uncomment. Es un Obj: meals
+console.log(callMealDBAPI(searchAllCats,callType));
 // ###
 
 let searchAllAreas='https://www.themealdb.com/api/json/v1/1/list.php?a=list';
 
+//.meals uncomment. Es un Obj: meals
+// console.log(callMealDBAPI(searchAllAreas,callType));
+
 // ###
 let searchAllIngredients='https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+//Los ID de ingrediente no son consecutivos algunos se saltan
+//.meals uncomment. Es un Obj: meals
+// console.log(callMealDBAPI(searchAllIngredients,callType));
 
 // ###
 // # Filter by main ingredient
@@ -103,18 +143,32 @@ let searchAllIngredients='https://www.themealdb.com/api/json/v1/1/list.php?i=lis
 let searchxMainIngredient='https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken';
 // let searchxMainIngredient='https://www.themealdb.com/api/json/v1/1/filter.php?i=garlic';
 
+// console.log(callMealDBAPI(searchxMainIngredient,callType));
+//.meals uncomment. Es un Obj: meals
+// callMealDBAPI(searchxMainIngredient,callType);
+
 // ###
 // # Filter by multi-ingredient (only available to $2+ Paypal supporters)
 let searchxMultiIngredient='https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast,garlic,salt';
+
+//.meals uncomment. Es un Obj: meals
+//Hasta que se pague la API mientras es un null
+// console.log(callMealDBAPI(searchxMultiIngredient,callType));
 
 // ###
 // # Filter by Category
 let searchxCat='https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood';
 
+//.meals uncomment. Es un Obj: meals
+// console.log(callMealDBAPI(searchxCat,callType));
+
 // ###
 // # Filter by Area
 
 let searchxArea='https://www.themealdb.com/api/json/v1/1/filter.php?a=Canadian';
+
+//.meals uncomment. Es un Obj: meals
+// console.log(callMealDBAPI(searchxArea,callType));
 
 // # Images
 // # Meal Thumbnail Images
@@ -129,38 +183,10 @@ let getUrlImage='https://www.themealdb.com/images/ingredients/Lime.png';
 
 // ###getUrlSmallImage
 let getThumbnailImageUrl='https://www.themealdb.com/images/ingredients/Lime-Small.png';
-// let output='';
 
-// console.log(callMealDBAPI(searchxMealName, callType));
-// callMealDBAPI(searchxMealName, callType);
-// console.log(meals);
-
-console.log(callMealDBAPI(listAllMealsx1stLetter,callType));
-
-// console.log(callMealDBAPI(searchMealxId,callType));
-
-// console.log(callMealDBAPI(searchSingleRandomMeal,callType));
-
-// console.log(callMealDBAPI(search10RandomMeals,callType));
-
-// console.log(callMealDBAPI(searchAllMealCats,callType));
-
-// console.log(callMealDBAPI(searchLatestMeal,callType));
-
-// console.log(callMealDBAPI(searchAllAreas,callType));
-
-// console.log(callMealDBAPI(searchAllIngredients,callType));
-
-// console.log(callMealDBAPI(searchxMainIngredient,callType));
-// callMealDBAPI(searchxMainIngredient,callType);
-
-//document.getElementById('json').innerHTML = output;
-// console.log(callMealDBAPI(searchxMultiIngredient,callType));
-
-// console.log(callMealDBAPI(searchxCat,callType));
-
-// console.log(callMealDBAPI(searchxArea,callType));
-
+//============================================================
+//No Funciona lo que sigue a fetched porque es otro tipo de obj
+// Son imagenes
 // console.log(callMealDBAPI(getUrlPreview,callType));
 
 // console.log('getUrlImage');
@@ -168,3 +194,6 @@ console.log(callMealDBAPI(listAllMealsx1stLetter,callType));
 
 // console.log('getUrlSmallImage');
 // console.log(callMealDBAPI(getThumbnailImageUrl,callType));
+
+
+
