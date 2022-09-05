8f5bc7fb56351ba4074632df1483b16ed78f01bd
Miguel Chavez <toonchavez8@gmail.com>
2022-09-04 23:30:20 -0500

    re added webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	experiments: {
+		asyncWebAssembly: true,
+	},
+	entry: {
+		main: "./src/index.js",
+	},
+	plugins: [
+		// plugins section
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+			chunks: ["main"], //agregarmos "vendor", antes de main cuando queremos cargar logica bootstrap
+		}),
+	],
+	module: {
+		rules: [
+			// rules for modules (configure loaders, parser options, etc.)
+
+			{
+				// html loader
+				test: /\.html$/,
+				loader: "html-loader",
+			},
+			{
+				// image loader
+				test: /\.(svg|png|jpe?g|jpg|gif)$/,
+				type: "asset/resource",
+			},
+		],
+	},
+};
+// testingt
